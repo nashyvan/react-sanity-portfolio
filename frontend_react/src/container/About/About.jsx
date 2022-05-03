e941e49aef8af249804cc39af0abe98f667c1073
Yarik Nashivan <yarik.nashivan@gmail.com>
2022-05-04 00:42:34 +0300

    sanity connect

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,15 +3,18 @@ import {motion} from "framer-motion";
 
 import {images} from "../../constants";
 import "./About.scss"
-
-const abouts = [
-    {title: 'Web Development', description: 'Description42', imgUrl: images.about01},
-    {title: 'Web Design', description: 'Description42', imgUrl: images.about02},
-    {title: 'UI/UX', description: 'Description42', imgUrl: images.about03},
-    {title: 'Web Animations', description: 'Description42', imgUrl: images.about04},
-];
+import {urlFor, client} from "../../client";
 
 const About = () => {
+    const [abouts, setAbouts] = useState([]);
+    useEffect(() => {
+        const query = '*[_type == "abouts"]';
+        client.fetch(query)
+            .then((data) => {
+                setAbouts(data);
+            })
+    }, []);
+
     return(
         <>
             <h2 className="head-text">
@@ -28,7 +31,7 @@ const About = () => {
                         className="app__profile-item"
                         key={about.title + index}
                     >
-                        <img src={about.imgUrl} alt={about.title}/>
+                        <img src={urlFor(about.imgUrl)} alt={about.title}/>
                         <h2 className="bold-text" style={{marginTop: 20}}>{about.title}</h2>
                         <p className="p-text" style={{marginTop: 10}}>{about.description}</p>
                     </motion.div>
